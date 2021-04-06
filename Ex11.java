809fcb3cc40578a7d435b0627fc7c0e422c19380
DESKTOP-NUOPUIF\ITPS <heansum1556@gmail.com>
2021-04-06 17:45:50 +0900

    16days Updates(4/06)

--- Ex11.java
+++ Ex11.java
@@ -8,7 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 
-public class Ex9 {
+public class Ex11 {
 	public static void main(String[] args) {
 		FileReader fr = null;
 		BufferedReader br = null;
@@ -17,10 +17,10 @@ public class Ex9 {
 		BufferedWriter bw = null;
 		
 		try {
-			fr = new FileReader("Ex9.java");
+			fr = new FileReader("Ex11.java");
 			br = new BufferedReader(fr);
 		
-			fw = new FileWriter("Ex9_import.bak");
+			fw = new FileWriter("Ex11_import.bak");
 			bw = new BufferedWriter(fw);
 			
 			String line = "";
@@ -30,13 +30,13 @@ public class Ex9 {
 				
 				// line 변수 내 import 문자열이 포함되어있는지 체크
 				// indexOf 메서드 -> 매개변수로 전달한 문자열이 해당 객체 내 어느 위치에 존재하는지 알려주는 메서드
-				// 매개변수로 전달한 문자열이 존재한다 
-				line.indexOf("import");
-				
-				if(line != null) {
+				// 매개변수로 전달한 문자열이 존재한다 -> 0이상의 값을 반환 / 매개변수로 전달한 문자열의 인덱스 번호
+				// 매개변수로 전달한 문자열이 존재하지 않는다 -> -1을 반환
+				if(line.indexOf("import") != -1) {
 					bw.write(i + " : " + line);
 					bw.newLine();
 				}
+				
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
